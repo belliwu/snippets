a84e198bfed089f2e0daafbf4eb8b0f1a0398b81
belliwu <belliwu56@gmail.com>
2025-12-07 12:51:52 +0800

    feat: update layout and create snippet form

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,8 +1,37 @@
-export default function SnippetsCreatedPage() {
+export default function SnippetCreatePage() {
   return (
-    <div>
-      <h1>Create a New Snippets</h1>
-      {/* Form or content for creating a new snipper goes here */}
-    </div>
+    <form>
+      <h3 className="font-bold m-3">Create a New Snippet</h3>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <label htmlFor="title" className="w-12">
+            Title:
+          </label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            className="border rounded px-2 w-full"
+          />
+        </div>
+        <div className="flex gap-4">
+          <label htmlFor="code" className="w-12">
+            code:
+          </label>
+          <input
+            type="text"
+            id="code"
+            name="code"
+            className="border rounded px-2 w-full"
+          />
+        </div>
+        <button
+          type="button"
+          className="font-bold rounded p-2 bg-blue-200 cursor-pointer"
+        >
+          Create
+        </button>
+      </div>
+    </form>
   );
 }
