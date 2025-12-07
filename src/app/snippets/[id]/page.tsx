b6e2bb126dace4923455db61774dd5b552e865ef
belliwu <belliwu56@gmail.com>
2025-12-07 23:06:56 +0800

    feat: add SnippetShowPage component for displaying individual snippets

---
+++ src/app/snippets/[id]/page.tsx
@@ -0,0 +1,27 @@
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+
+interface SnippetProps {
+  params: Promise<{
+    id: string;
+  }>;
+}
+
+export default async function SnippetShowPage(props: SnippetProps) {
+  const { id } = await props.params;
+
+  const snippet = await db.snippet.findFirst({
+    where: { id: parseInt(id) },
+  });
+
+  if (!snippet) {
+    return notFound();
+  }
+  console.log(snippet);
+
+  return (
+    <div>
+      <h1>{snippet.title}</h1>
+    </div>
+  );
+}
