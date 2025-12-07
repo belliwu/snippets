2c5021e37a0a15c7c657088d11d86b4d2faf7222
belliwu <belliwu56@gmail.com>
2025-12-07 12:32:35 +0800

    feat: add Prisma setup and Snippet model
    - Added Prisma client and dotenv to dependencies.
    - Created Prisma configuration file and initialized SQLite database.
    - Defined Snippet model with id, title, and code fields in schema.prisma.
    - Added migration for creating Snippet table.
    - Created new page for adding snippets with a placeholder for the form.
    - Updated layout and home page to simplify structure and content.

---
+++ src/app/snippets/new/page.tsx
@@ -0,0 +1,8 @@
+export default function SnippetsCreatedPage() {
+  return (
+    <div>
+      <h1>Create a New Snippets</h1>
+      {/* Form or content for creating a new snipper goes here */}
+    </div>
+  );
+}
